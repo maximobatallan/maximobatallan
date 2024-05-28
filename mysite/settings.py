cc638f4b4471d5c1d23656fe1e6de6e64b83f187
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2024-05-28 16:39:38 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TAtqxJWSEMAybkIkJyTmNmzYqfGDWFCy',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '46503',
     }
 }
 
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_HOST_USER = 'notificaciondepaginaweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'iddaknkbspmdhanl'

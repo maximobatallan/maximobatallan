4270b1da351c62d7493432332f70d7fffda71ae9
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2024-05-28 17:09:03 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from mb import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('formulario.html', views.save_formulario, name='save_formulario'),
     path('asistentevirtual.html', views.asistentevirtual, name='asistentevirtual'),
     path('desarrollospersonalizados.html', views.desarrollospersonalizados, name='desarrollospersonalizados'),
     path('exposicionenlinea.html', views.exposicionenlinea, name='exposicionenlinea'),
